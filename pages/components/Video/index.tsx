4c0972131779570c30c9a30393388b010927d6b8
Sourav Raj <souravrajput@Sourav-ka-Air.domain.name>
2023-05-31 04:44:46 +0530

    video

--- pages/components/Video/index.tsx
+++ pages/components/Video/index.tsx
@@ -4,11 +4,52 @@ const Video = () => {
   return (
 
     <>
-    <div className="flex flex-col items-center">
-      <h2 className="text-2xl font-bold mb-4">Video Testimonial</h2>
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/bh5rNAQpLZM" title="Bareilly ke Bazaar starring Ishita Raj - Sreenivas B, Tanishk B | Sunidhi Chauhan, Dev Negi"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-    </div>
+    
 
+
+
+<div className="flex  flex-col items-center justify-center space-y-6 bg-gray-100 px-4 sm:flex-row sm:space-x-6 sm:space-y-0">
+        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-xl">
+        <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden" style={{ paddingTop: '56.25%' }}>
+      <iframe
+        className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+        src="https://www.youtube.com/embed/FQWYjJ4GSsY"
+        allowFullScreen
+      ></iframe>
+    </div>
+          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">Satisfied Client of Grandeur Net</h1>
+          <p className="my-4 text-center text-sm text-gray-500">Baby Product Brand</p>
+          
+        </div>
+
+
+        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-xl">
+        <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden" style={{ paddingTop: '56.25%' }}>
+      <iframe
+        className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+        src="https://www.youtube.com/embed/N5aZPiuRovo"
+        allowFullScreen
+      ></iframe>
+    </div>
+          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">Satisfied Client of Grandeur Net</h1>
+          <p className="my-4 text-center text-sm text-gray-500">Salon , Gym , Spa</p>
+          
+        </div>
+
+        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-xl">
+        <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden" style={{ paddingTop: '56.25%' }}>
+      <iframe
+        className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+        src="https://www.youtube.com/embed/wGHPbn6_VJY"
+        allowFullScreen
+      ></iframe>
+    </div>
+          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">Satisfied Client of Grandeur Net</h1>
+          <p className="my-4 text-center text-sm text-gray-500">Construction Company</p>
+         
+        </div>
+      </div>
+      
 </>
 
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,7 @@ const Home = () => {
         <Manage />
         <FAQ />
         <Testimonials />
+        
         <Video/>
         <Articles />
         <Joinus />
